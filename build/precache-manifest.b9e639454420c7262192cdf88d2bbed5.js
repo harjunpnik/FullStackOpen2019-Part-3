b1a8d70593b7bba966e3d938e05464f3bc083ac3
harjunpnik <niklasharjunpaa@gmail.com>
2019-09-02 17:38:04 +0300

    Excercise 3.11 Finished

---
+++ build/precache-manifest.b9e639454420c7262192cdf88d2bbed5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6c8819db8dd627898a6a954797b3cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d3caf148d6220dbf960",
+    "url": "/static/css/main.a4de3c9d.chunk.css"
+  },
+  {
+    "revision": "e531ad2ce5ea044f30ae",
+    "url": "/static/js/2.19635263.chunk.js"
+  },
+  {
+    "revision": "8d3caf148d6220dbf960",
+    "url": "/static/js/main.485e37b5.chunk.js"
+  },
+  {
+    "revision": "643b1496a8e20922303c",
+    "url": "/static/js/runtime~main.bf59cc56.js"
+  }
+]);
